c36351d0bf5fb2d68e089f36785f10c0f7b323c9
monochromer <monochromer@users.noreply.github.com>
2019-12-23 23:59:03 +0500

    add `thenable`

---
+++ thenable.js
@@ -0,0 +1,32 @@
+export class Thenable {
+  constructor() {
+    this.next = null;
+  }
+
+  then(resolve, reject) {
+    this.resolve = resolve;
+    this.reject = reject;
+    return this.next = new Thenable();
+  }
+
+  reject(error) {
+    throw typeof error === 'string' ? new Error(error) : error
+  }
+
+  resolve(value) {
+    if (this.resolve) {
+      try {
+        const next = this.resolve(value);
+        if (next) {
+          next.then(value => {
+            this.next.resolve(value);
+          });
+        }
+      } catch (error) {
+        if (this.reject) {
+          this.reject(error);
+        }
+      }
+    }
+  }
+}
